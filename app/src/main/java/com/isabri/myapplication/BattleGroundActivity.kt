ab1ca3a75537a6b4495483c98272a6d30873e245
Ismael Sabri <hisme14@gmail.com>
2022-11-22 12:08:52 +0100

    Commit for fragment issue revision

--- app/src/main/java/com/isabri/myapplication/BattleGroundActivity.kt
+++ app/src/main/java/com/isabri/myapplication/BattleGroundActivity.kt
@@ -2,16 +2,23 @@ package com.isabri.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import com.isabri.myapplication.databinding.ActivityBattleGroundBinding
+import com.isabri.myapplication.ui.heroeslist.HeroesListFragment
 
 class BattleGroundActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityBattleGroundBinding
+    lateinit var binding: ActivityBattleGroundBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityBattleGroundBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(binding.container.id, HeroesListFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/isabri/myapplication/ui/heroeslist/HeroesListViewModel.kt
@@ -0,0 +1,7 @@
+package com.isabri.myapplication.ui.heroeslist
+
+import androidx.lifecycle.ViewModel
+
+class HeroesListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
